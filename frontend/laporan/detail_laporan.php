a2da1414dd68fda8e8884db053a695f0b10086a2
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-20 01:45:46 +0700

    rencana tampilan laporan

---
+++ frontend/laporan/detail_laporan.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Monitoring Biaya Produksi | Beranda</title>
+
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+		<link
+			rel="stylesheet"
+			href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css"
+		/>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+		/>
+		<link rel="stylesheet" href="../../assets/css/main.css" />
+		<link rel="stylesheet" href="../../assets/css/style.css" />
+		<link rel="stylesheet" href="../../assets/css/sidebar.css" />
+	</head>
+	<body>
+	<?php
+		include '../../database/koneksi.php';
+		include '../../frontend/layout/sidebar.php';
+	?>
+		<main class="main">
+			<section>
+				<h2>Monitoring Biaya Produksi</h2>
+				<h3>Laporan Barang Keluar</h3>
+				<div class="breadcrumb">
+          <h3>
+						<a href="../../beranda.php?page=beranda">Beranda</a> <i class="fa fa-angle-right"></i>
+						<a href="index.php?page=laporan">Data Laporan Barang Keluar</a> <i class="fa fa-angle-right"></i>
+						<span class="akhir-link-breadcrumb">Detail Laporan Barang Keluar</span>
+					</h3>
+				</div>
+
+				<div class="container margin-bottom-100">
+					<div class="baris">
+            <div class="kolom-50-kiri">
+              <div class="box-konten">
+                <center>
+                  <div class="box-header-radius-80 background-biru teks-putih margin-bottom-50">
+                    <h3>Data Transaksi</h3>
+                  </div>
+                </center>
+
+                <?php
+
+                  $Tr                    = $_GET['Tr'];
+                  $sqlSelectTransaksi    = "SELECT * FROM transaksi WHERE no_transaksi = '$Tr' ";
+                  $querySelectTransaksi  = mysqli_query($host, $sqlSelectTransaksi);
+                  $DetailTransaksi       = mysqli_fetch_assoc($querySelectTransaksi);
+
+                ?>
+                <form>
+                  <label>Nama Pembeli</label>
+                  <input type="text" class="form" value="<?= $DetailTransaksi['nama_pembeli'] ?>" readonly>
+
+                  <label>Nomor Transaksi</label>
+                  <input type="text" class="form" value="<?= $DetailTransaksi['no_transaksi'] ?>" readonly>
+
+                  <label>Keterangan Transaksi</label>
+                  <label>Keterangan</label>
+                  <textarea class="form textarea-no-resize" rows="5" readonly><?= $DetailTransaksi['keterangan'] ?></textarea>
+
+                  <label>Petugas Kasir</label>
+                  <input type="text" class="form" value="<?= $DetailTransaksi['nama_kasir'] ?>" readonly>
+                </form>
+              </div>
+            </div>
+            </div>
+
+            <div class="kolom-100 margin-top-50">
+              <center>
+                <div class="box-header-radius-80 background-biru teks-putih margin-bottom-50">
+                  <h3>Barang Yang Di Beli</h3>
+                </div>
+              </center>
+              <div class="table-box">
+                <table class="table-responsive">
+                  <tr class="thead-dark">
+                    <th>No</th>
+                    <th>Nama Barang</th>
+                    <th>Qty</th>
+                    <th>Sub Total</th>
+                  </tr>
+
+                  <?php
+
+                    $no                = 1;
+                    $sqlDetailBarang   = "SELECT * FROM detail_transaksi INNER JOIN barang ON detail_transaksi.barang_id = barang.id_barang WHERE nomor_tr = '$Tr' ";
+                    $queryDetailBarang = mysqli_query($host, $sqlDetailBarang);
+                    while ($DetailBarang = mysqli_fetch_assoc($queryDetailBarang)) {
+
+                  ?>
+                  <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= $DetailBarang['nama_barang'] ?></td>
+                    <td><?= $DetailBarang['qty'] . " " . $DetailBarang['satuan_stok_barang'] ?></td>
+                    <td><?= "Rp. " . number_format($DetailBarang['sub_total'],0,',','.') ?></td>
+                  </tr>
+                  <?php } ?>
+
+                  <?php
+
+                    $sqlDetailBarangSub   = "SELECT SUM(sub_total) AS sub_total FROM detail_transaksi WHERE nomor_tr = '$Tr' ";
+                    $queryDetailBarangSub = mysqli_query($host, $sqlDetailBarangSub);
+                    $dataSubDetail_barang = mysqli_fetch_assoc($queryDetailBarangSub);
+
+                  ?>
+                  <tr>
+                    <td colspan="3">Total Pembelian</td>
+                    <td class="background-hijau teks-putih">
+                      <?= "Rp. " . number_format($dataSubDetail_barang['sub_total'],0,',','.') ?>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <a href="index.php?page=laporan" class="tmbl tmbl-abu-abu margin-20-0">
+                Kembali
+              </a>
+            </div>
+          </div>
+				</div>
+				
+      </section>
+
+		</main>
+	</body>
+</html>
---
+++ frontend/laporan/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Monitoring Biaya Produksi | Beranda</title>
+
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+		<link
+			rel="stylesheet"
+			href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css"
+		/>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+		/>
+		<link rel="stylesheet" href="../../assets/css/main.css" />
+		<link rel="stylesheet" href="../../assets/css/style.css" />
+		<link rel="stylesheet" href="../../assets/css/sidebar.css" />
+	</head>
+	<body>
+	<?php
+		include '../../database/koneksi.php';
+		include '../../frontend/layout/sidebar.php';
+	?>
+		<main class="main">
+			<section>
+				<h2>Monitoring Biaya Produksi</h2>
+				<h3>Laporan Barang Keluar</h3>
+				<div class="breadcrumb">
+          <h3>
+						<a href="../../beranda.php?page=beranda">Beranda</a> <i class="fa fa-angle-right"></i>
+						<span class="akhir-link-breadcrumb">Data Laporan Barang Keluar</span>
+					</h3>
+				</div>
+
+				<div class="container margin-bottom-100">
+					<div class="baris baris-tengah">
+            <div class="kolom-50">
+              <div class="box-konten-radius padding-20 backgorund-e7">
+                <center>
+                  <div class="box-header-radius-80 background-biru teks-putih margin-bottom-50">
+                    <h3>Tampilkan Laporan Berdasarkan Tanggal</h3>
+                  </div>
+                </center>
+
+                <!-- Trigger/Open The Modal -->
+                <center>
+                  <button id="myBtn" class="tmbl tmbl-abu-abu">Pilih Tanggal</button>
+                </center>
+
+                <!-- The Modal -->
+                <div id="myModal" class="modal">
+
+                  <!-- Modal content -->
+                  <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+            <div class="kolom-100 margin-top-50">
+              <div class="table-box">
+                <table class="table-responsive">
+                  <tr class="thead-dark">
+                    <th>No</th>
+                    <th>Tanggal Pembelian</th>
+                    <th>Nomor Nota</th>
+                    <th>Petugas Kasir</th>
+                    <th>Aksi</th>
+                  </tr>
+
+                  <?php
+
+                    $no                 = 1;
+                    $sqlSelectLaporan   = "SELECT * FROM laporan_brg_keluar";
+                    $querySelectLaporan = mysqli_query($host, $sqlSelectLaporan);
+                    while ($dataLaporan = mysqli_fetch_assoc($querySelectLaporan)) {
+
+                  ?>
+                  <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= date('d M Y', strtotime($dataLaporan['tgl_laporan']))  ?></td>
+                    <td><?= $dataLaporan['nomor_transaksi'] ?></td>
+                    <td><?= $dataLaporan['petugas_kasir'] ?></td>
+                    <td>
+                      <a href="detail_laporan.php?Tr=<?= $dataLaporan['nomor_transaksi'] ?>&page=laporan" class="tmbl tmbl-biru">
+                        <i class="fa fa-eye"></i>
+                      </a>
+                      <a href="" class="tmbl tmbl-hijau">
+                        <i class="fa fa-clipboard"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <?php } ?>
+                </table>
+              </div>
+            </div>
+          </div>
+				</div>
+				
+      </section>
+
+		</main>
+
+
+    <!-- Javascript -->
+    <script>
+      // Get the modal
+      var modal = document.getElementById("myModal");
+
+      // Get the button that opens the modal
+      var btn = document.getElementById("myBtn");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks the button, open the modal 
+      btn.onclick = function() {
+        modal.style.display = "block";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    </script>
+
+	</body>
+</html>
