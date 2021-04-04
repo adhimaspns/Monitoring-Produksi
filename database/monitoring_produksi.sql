fd4b7b99737ac5e4872795587b715cb918183cd9
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-04-04 23:38:53 +0700

    update sql

--- database/monitoring_produksi.sql
+++ database/monitoring_produksi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Apr 2021 pada 12.36
+-- Waktu pembuatan: 04 Apr 2021 pada 23.37
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.3
 
@@ -66,17 +66,18 @@ CREATE TABLE `barang` (
   `satuan_stok_barang` varchar(100) NOT NULL,
   `untung_barang` int(100) NOT NULL,
   `harga_jual_item` int(20) NOT NULL,
-  `produksi_id` varchar(200) NOT NULL
+  `produksi_id` varchar(200) NOT NULL,
+  `tanggal_produksi` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`id_barang`, `nama_barang`, `stok_barang`, `satuan_stok_barang`, `untung_barang`, `harga_jual_item`, `produksi_id`) VALUES
-(10, 'Cowek', 0, 'Pcs', 1000, 1835, '93'),
-(11, 'Kendil', 0, 'Pcs', 1000, 4129, '94'),
-(12, 'Cowek', 740, 'Pcs', 1500, 2185, '95');
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `stok_barang`, `satuan_stok_barang`, `untung_barang`, `harga_jual_item`, `produksi_id`, `tanggal_produksi`) VALUES
+(10, 'Cowek', 0, 'Pcs', 1000, 1835, '93', '2021-03-19'),
+(11, 'Kendil', 0, 'Pcs', 1000, 4129, '94', '2021-03-19'),
+(12, 'Cowek', 730, 'Pcs', 1000, 1685, '95', '2021-04-22');
 
 -- --------------------------------------------------------
 
@@ -115,7 +116,8 @@ INSERT INTO `detail_produksi` (`id_detail_produksi`, `produksi_id`, `bhn_id`, `q
 (98, 95, 18, 4, 60000),
 (99, 95, 19, 1, 75000),
 (100, 95, 20, 2, 200000),
-(101, 95, 25, 3, 150000);
+(101, 95, 25, 3, 150000),
+(108, 96, 23, 380, 114000);
 
 -- --------------------------------------------------------
 
@@ -149,7 +151,8 @@ INSERT INTO `detail_transaksi` (`id_detail_transaksi`, `tgl_transaksi`, `nomor_t
 (74, '2021-03-24', '202103240001', '12', 20, 2185, 30000, 43700),
 (75, '2021-03-31', '202103310001', '12', 100, 2185, 150000, 218500),
 (77, '2021-04-03', '202104030001', '12', 10, 2185, 15000, 21850),
-(78, '2021-04-03', '202104030002', '12', 30, 2185, 45000, 65550);
+(78, '2021-04-03', '202104030002', '12', 30, 2185, 45000, 65550),
+(79, '2021-04-04', '202104040001', '12', 10, 1685, 10000, 16850);
 
 -- --------------------------------------------------------
 
@@ -196,7 +199,8 @@ INSERT INTO `laporan_brg_keluar` (`id_laporan`, `tgl_laporan`, `nomor_transaksi`
 (12, '2021-03-24', '202103240001', 43700, 'Adhimas Pns', 'Beli Cowek'),
 (13, '2021-03-31', '202103310001', 218500, 'Adhimas Pns', 'Beli Cowek & Kendil'),
 (14, '2021-04-03', '202104030001', 21850, 'Adhimas Pns', 'Beli Cowek 10'),
-(15, '2021-04-03', '202104030002', 65550, 'Adhimas Pns', 'Beli Cowek 30');
+(15, '2021-04-03', '202104030002', 65550, 'Adhimas Pns', 'Beli Cowek 30'),
+(16, '2021-04-04', '202104040001', 16850, 'Adhimas Pns', 'Beli Cowek');
 
 -- --------------------------------------------------------
 
@@ -221,7 +225,7 @@ CREATE TABLE `produksi` (
 INSERT INTO `produksi` (`id_produksi`, `nama_produk`, `stok_produk`, `satuan_stok_produk`, `untung_produk`, `harga_jual`, `tgl_produksi`) VALUES
 (93, 'Cowek', 1000, 'Pcs', 1000, 1835, '2021-03-19'),
 (94, 'Kendil', 380, 'Pcs', 1000, 4129, '2021-03-19'),
-(95, 'Cowek', 1000, 'Pcs', 1500, 2185, '2021-03-22');
+(95, 'Cowek', 1000, 'Pcs', 1000, 1685, '2021-03-22');
 
 -- --------------------------------------------------------
 
@@ -265,7 +269,8 @@ INSERT INTO `transaksi` (`id_tbl_transaksi`, `no_transaksi`, `nama_pembeli`, `ke
 (30, '202103240001', 'Nur Cahyo', 'Beli Cowek', 'Adhimas Pns'),
 (31, '202103310001', 'Putri Nova', 'Beli Cowek & Kendil', 'Adhimas Pns'),
 (32, '202104030001', 'Suprapto', 'Beli Cowek 10', 'Adhimas Pns'),
-(33, '202104030002', 'Sugianto', 'Beli Cowek 30', 'Adhimas Pns');
+(33, '202104030002', 'Sugianto', 'Beli Cowek 30', 'Adhimas Pns'),
+(34, '202104040001', 'Nur Samsu', 'Beli Cowek', 'Adhimas Pns');
 
 -- --------------------------------------------------------
 
@@ -367,49 +372,49 @@ ALTER TABLE `bahan`
 -- AUTO_INCREMENT untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_barang` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `detail_produksi`
 --
 ALTER TABLE `detail_produksi`
-  MODIFY `id_detail_produksi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id_detail_produksi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- AUTO_INCREMENT untuk tabel `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT untuk tabel `kasir`
 --
 ALTER TABLE `kasir`
-  MODIFY `id_kasir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id_kasir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- AUTO_INCREMENT untuk tabel `laporan_brg_keluar`
 --
 ALTER TABLE `laporan_brg_keluar`
-  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT untuk tabel `produksi`
 --
 ALTER TABLE `produksi`
-  MODIFY `id_produksi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id_produksi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT untuk tabel `temp`
 --
 ALTER TABLE `temp`
-  MODIFY `id_temp` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=192;
+  MODIFY `id_temp` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_tbl_transaksi` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_tbl_transaksi` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
