acdc56abcd2b41e90f17e30e0c9b782dda8ff038
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-04-04 21:06:50 +0700

    merubah title

--- frontend/produksi/index.php
+++ frontend/produksi/index.php
@@ -12,7 +12,7 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Monitoring Biaya Produksi | Data Jasa</title>
+		<title>Monitoring Biaya Produksi | Data Produksi</title>
 
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
