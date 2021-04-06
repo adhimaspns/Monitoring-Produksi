c880c4647e3bdc864988137610263f5de79d31b6
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-04-07 00:45:18 +0700

    menambahkan validasi jika user memilih 0

--- backend/temp/tambah_bahan.php
+++ backend/temp/tambah_bahan.php
@@ -153,69 +153,74 @@
     $sqlCek   = mysqli_query($host, "SELECT * FROM temp WHERE bhn_id = '$bhn_id' AND produksi_id= '$id_produksi'");
     $cekId    = mysqli_num_rows($sqlCek);
 
-    if ($cekId != 0) {
-
-      //! Akan dijalankan jika ada data dengan id sesuai yang di dapat dari $cekId
-      $qtyAwal   = mysqli_fetch_assoc($sqlCek); // menampilkan qty awal dari tabel temp
-      $qtyAkhir  = $qtyAwal['qty'] + $jumlah_qty;  // menambahkan qty jika data barang ada pada tabel temp
-
-
-      //! QUERY Update Qty Temp
-      $sqlUpdate = "UPDATE temp SET qty = '$qtyAkhir' WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
-      $query     = mysqli_query($host, $sqlUpdate);
-
-
-      //! QUERY Update Qty Produksi
-      $sqlUpdate = "UPDATE detail_produksi SET qty = '$qtyAkhir' WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
-      $query     = mysqli_query($host, $sqlUpdate);
-
-
-      //! QUERY Select subtotal
-      $sqlSelectSubTotal   = "SELECT * FROM temp INNER JOIN bahan ON temp.bhn_id = bahan.id_bahan WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
-      $querySelectSubTotal = mysqli_query($host, $sqlSelectSubTotal);
-      $data_sub_total      = mysqli_fetch_assoc($querySelectSubTotal);
-      $harga               = $data_sub_total['harga'];
-      $subtotal            = $qtyAkhir*$harga;
-
-
-      //! UPDATE TEMP 
-      $sqlSub  = mysqli_query($host, "UPDATE temp SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' ");
-      //! UPDATE PRODUKSI 
-      $sqlSub  = mysqli_query($host, "UPDATE detail_produksi SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' ");
-
+    if ($jumlah_qty == 0) {
+      echo "<script>alert('Masukkan jumlah yang sesuai!');window.location.href='../../frontend/produksi/produksi.php?id_produksi=$id_produksi&page=produksi'</script>";
     } else {
-      //! Jika data tidak ditemukan, maka akan menambahkan data baru 
 
-      //! SELECT ID PRODUKSI
-      $selectIdProduksi     = "SELECT * FROM produksi ORDER BY id_produksi DESC LIMIT 1";
-      $queryIdProduksi      = mysqli_query($host, $selectIdProduksi);
-      $dataProduksi         = mysqli_fetch_assoc($queryIdProduksi);
-      $id_produksi          = $dataProduksi['id_produksi'];
+      if ($cekId != 0) {
 
-      //! QUERY INSERT TEMP 
-      $sqlInsert  = "INSERT INTO temp VALUES('', '$id_produksi', '$bhn_id', '$jumlah_qty', 0)"; 
-      $query      = mysqli_query($host, $sqlInsert);
+        //! Akan dijalankan jika ada data dengan id sesuai yang di dapat dari $cekId
+        $qtyAwal   = mysqli_fetch_assoc($sqlCek); // menampilkan qty awal dari tabel temp
+        $qtyAkhir  = $qtyAwal['qty'] + $jumlah_qty;  // menambahkan qty jika data barang ada pada tabel temp
 
 
-      //! INSERT DETAIL PRODUKSI
-      $sqlDetailProdusi = "INSERT INTO detail_produksi VALUES(0, '$id_produksi', '$bhn_id', '$jumlah_qty', 0)";
-      $queryDetail      = mysqli_query($host, $sqlDetailProdusi);
+        //! QUERY Update Qty Temp
+        $sqlUpdate = "UPDATE temp SET qty = '$qtyAkhir' WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
+        $query     = mysqli_query($host, $sqlUpdate);
+
+
+        //! QUERY Update Qty Produksi
+        $sqlUpdate = "UPDATE detail_produksi SET qty = '$qtyAkhir' WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
+        $query     = mysqli_query($host, $sqlUpdate);
+
+
+        //! QUERY Select subtotal
+        $sqlSelectSubTotal   = "SELECT * FROM temp INNER JOIN bahan ON temp.bhn_id = bahan.id_bahan WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
+        $querySelectSubTotal = mysqli_query($host, $sqlSelectSubTotal);
+        $data_sub_total      = mysqli_fetch_assoc($querySelectSubTotal);
+        $harga               = $data_sub_total['harga'];
+        $subtotal            = $qtyAkhir*$harga;
+
+
+        //! UPDATE TEMP 
+        $sqlSub  = mysqli_query($host, "UPDATE temp SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' ");
+        //! UPDATE PRODUKSI 
+        $sqlSub  = mysqli_query($host, "UPDATE detail_produksi SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' ");
+
+      } else {
+        //! Jika data tidak ditemukan, maka akan menambahkan data baru 
+
+        //! SELECT ID PRODUKSI
+        $selectIdProduksi     = "SELECT * FROM produksi ORDER BY id_produksi DESC LIMIT 1";
+        $queryIdProduksi      = mysqli_query($host, $selectIdProduksi);
+        $dataProduksi         = mysqli_fetch_assoc($queryIdProduksi);
+        $id_produksi          = $dataProduksi['id_produksi'];
+
+        //! QUERY INSERT TEMP 
+        $sqlInsert  = "INSERT INTO temp VALUES('', '$id_produksi', '$bhn_id', '$jumlah_qty', 0)"; 
+        $query      = mysqli_query($host, $sqlInsert);
+
+
+        //! INSERT DETAIL PRODUKSI
+        $sqlDetailProdusi = "INSERT INTO detail_produksi VALUES(0, '$id_produksi', '$bhn_id', '$jumlah_qty', 0)";
+        $queryDetail      = mysqli_query($host, $sqlDetailProdusi);
 
 
 
-      //! QUERY Select subtotal
-      $sqlSelectSubTotal   = "SELECT * FROM temp INNER JOIN bahan ON temp.bhn_id = bahan.id_bahan WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
-      $querySelectSubTotal = mysqli_query($host, $sqlSelectSubTotal);
-      $data_sub_total      = mysqli_fetch_assoc($querySelectSubTotal);
-      $harga               = $data_sub_total['harga'];
-      $subtotal            = $jumlah_qty*$harga;
+        //! QUERY Select subtotal
+        $sqlSelectSubTotal   = "SELECT * FROM temp INNER JOIN bahan ON temp.bhn_id = bahan.id_bahan WHERE bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ";
+        $querySelectSubTotal = mysqli_query($host, $sqlSelectSubTotal);
+        $data_sub_total      = mysqli_fetch_assoc($querySelectSubTotal);
+        $harga               = $data_sub_total['harga'];
+        $subtotal            = $jumlah_qty*$harga;
 
 
-      //! QUERY Update Subtotal Temp 
-      $sqlSub          = mysqli_query($host, "UPDATE temp SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ");
-      //! QUERY Update Subtotal Produksi
-      $sqlSubProduksi  = mysqli_query($host, "UPDATE detail_produksi SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' AND produksi_id = '$id_produksi'  ");
+        //! QUERY Update Subtotal Temp 
+        $sqlSub          = mysqli_query($host, "UPDATE temp SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' AND produksi_id = '$id_produksi' ");
+        //! QUERY Update Subtotal Produksi
+        $sqlSubProduksi  = mysqli_query($host, "UPDATE detail_produksi SET sub_total = '$subtotal' WHERE  bhn_id = '$bhn_id' AND produksi_id = '$id_produksi'  ");
 
+      }
     }
 
     if($query){
